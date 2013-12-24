7c28628d225daddd6f8bcf68b5ce44b0006cf47c
Grandborn <jbluettduncan@gmail.com>
2013-12-24 01:09:00 +0000

    Created AllTests

---
+++ java-textualyser/test/uk/co/bluettduncanj/AllTests.java
@@ -0,0 +1,20 @@
+/**
+ * AllTests.java
+ */
+
+package uk.co.bluettduncanj;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * @author Jonathan
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ AbstractFileHandlerTest.class, BoyerMooreTest.class, FileAnalyserTest.class, GUITest.class,
+    LogFileHandlerTest.class, ReqsTest.class, StatisticsTest.class })
+public class AllTests {
+
+}
